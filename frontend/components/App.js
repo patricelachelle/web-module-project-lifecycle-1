877497cb93e6079c3643ba341ab4383013ac6545
patricelachelle <79610155+patricelachelle@users.noreply.github.com>
2022-03-11 18:02:47 -0600

    fetch

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,14 +1,35 @@
+import axios from 'axios'
 import React from 'react'
 
+const URL = 'http://localhost:9000/api/todos'
+
 export default class App extends React.Component {
+  state = {
+    todos: [],
+  }
+  fetchAllTodos = () => {
+    axios.get(URL)
+      .then(res => {
+        this.setState({ ...this.state, todos: res.data.data })
+      })
+      .catch(err => {
+       this.setState({ ...this.state, error: err.response.data.message })
+      })
+  }
+  componentDidMount() {
+    this.fetchAllTodos()
+  }
   render() {
     return (
       <div>
         <div id='error'>Error: No error here</div>
         <div id='todos'>
           <h2>Todos:</h2>
-          <div>Walk the dog</div>
-          <div>Learn React</div>
+          {
+            this.state.todos.map(todo => {
+              return <div key={todo.id}>{todo.name}</div>
+            })
+          }
         </div>
         <form id='todoForm'>
           <input type='text' placeholder='Type todo'></input>
